2cfd182b29f55d97e75e0ecb8f3df280b4eba2e3
Munkhbilguun Munkhdemberel <moondiidev@gmail.com>
2022-11-04 16:11:32 +0800

    added a test rocket and simple movement

--- Assets/Scenes/Levels/L2/Scripts/RocketMovement.cs
+++ Assets/Scenes/Levels/L2/Scripts/RocketMovement.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class RocketMovement : MonoBehaviour
 {
-    Rigidbody2D rigBody;
-    public float movementSpeed = 1f;
-    // Start is called before the first frame update
+    public float acceleration = 20f;
+
     void Start()
     {
-        rigBody = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate(){
-        rigBody.velocity = new Vector2(0, movementSpeed);
+        transform.Translate(Vector2.up * (Time.deltaTime * acceleration));
     }
 }
