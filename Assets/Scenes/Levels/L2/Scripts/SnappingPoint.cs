2d1d6e5b709f6acb684ecb32690f718f929c73c8
Eesaa Philips <32400503+Ephilipz@users.noreply.github.com>
2023-03-23 06:38:28 +0300

    Delete Assets directory

--- Assets/Scenes/Levels/L2/Scripts/SnappingPoint.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SnappingPoint : MonoBehaviour
-{
-    public string direction; // left, right, top, or bottom
-    public bool isAttached = false;
-}
