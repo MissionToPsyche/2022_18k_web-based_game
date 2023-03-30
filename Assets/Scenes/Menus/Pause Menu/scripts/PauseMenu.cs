1221b08580d0dc651cdc10fd3df5a03f3fc3e328
Eesaa Philips <eesaa2000@gmail.com>
2023-03-30 12:07:06 +0300

    Pause Menu in all scenes

--- Assets/Scenes/Menus/Pause Menu/scripts/PauseMenu.cs
+++ Assets/Scenes/Menus/Pause Menu/scripts/PauseMenu.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,10 +10,12 @@ public class PauseMenu : MonoBehaviour
 {
     GameObject canvas;
     private AudioSource[] audioSources;
+    private Canvas[] canvases;
     // Start is called before the first frame update
     void Start()
     {
         audioSources = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+        canvases = FindObjectsOfType(typeof(Canvas)) as Canvas[];
         canvas = gameObject.transform.GetChild(0).gameObject;
         canvas.SetActive(false);
     }
@@ -19,6 +23,12 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (canvas.activeInHierarchy)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
         if (!Input.GetKeyDown(KeyCode.Escape))
         {
             return;
@@ -37,6 +47,7 @@ public class PauseMenu : MonoBehaviour
     {
         canvas.SetActive(true);
         setAllAudio(false);
+        setAllCanvas(false);
         Time.timeScale = 0f;
     }
 
@@ -48,10 +59,24 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
+    private void setAllCanvas(bool enabled)
+    {
+        foreach (Canvas c in canvases)
+        {
+            bool isPuaseMenuCanvas = c.GetComponentInParent<PauseMenu>() != null;
+            if (isPuaseMenuCanvas)
+            {
+                continue;
+            }
+            c.enabled = enabled;
+        }
+    }
+
     public void Resume()
     {
         canvas.SetActive(false);
         setAllAudio(true);
+        setAllCanvas(true);
         Time.timeScale = 1f;
     }
 
