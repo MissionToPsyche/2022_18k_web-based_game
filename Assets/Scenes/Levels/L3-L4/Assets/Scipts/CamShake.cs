fa9bf8199a173f75ea6b7cff46be3498e7beb06c
Chansen Brown <ChansenBrown@gmail.com>
2023-04-18 11:44:59 -0400

    add cam shake fix

---
+++ Assets/Scenes/Levels/L3-L4/Assets/Scipts/CamShake.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamShake : MonoBehaviour
+{
+
+    
+    public IEnumerator Shake (float duration, float magnitude)
+    {
+        Vector3 originalPos = transform.localPosition;
+        
+
+        float elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1.5f, 1.5f) * magnitude;
+            float y = Random.Range(-1.5f, 1.5f) * magnitude;
+
+            
+
+            transform.localPosition = new Vector3(x, y, originalPos.z);
+            
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.localPosition = originalPos;
+        
+    }
+}
